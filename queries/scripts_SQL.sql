f76d4fdb3da805b330940eb13aba51abd679a538
Jbaruz <jbaruz@gmail.com>
2022-08-25 17:11:31 -0500

    Agregue README.md

---
+++ queries/scripts_SQL.sql
@@ -0,0 +1,6 @@
+SELECT * FROM users; -- Consulta todos los datos de la tabla usuarios
+SELECT * FROM follows; -- Consulta todos los datos de la tabla follows
+SELECT * FROM faves;
+SELECT * FROM tweets;
+
+SELECT id, first_name FROM users; -- Separando por comas, especifica las columnas del select
